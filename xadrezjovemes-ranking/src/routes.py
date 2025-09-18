26f753b9c6639a0706cc0bb0209cdbb799e42bb5
Gedeon-Arruda <gd.deonnn@hotmail.com>
2025-09-18 17:01:29 -0300

    teste 2

--- xadrezjovemes-ranking/src/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from .ranking import load_players, PLAYERS
-
-bp = Blueprint('main', __name__)
-
-@bp.route('/')
-def index():
-    load_players()
-    return render_template('index.html', players=PLAYERS)
